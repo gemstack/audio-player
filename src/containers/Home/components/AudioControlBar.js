db56e5ce5e724987bb7aeee79f0bbdc68cee8395
gemstack18 <gemstack18@gmail.com>
2019-08-13 20:23:58 +0530

    Update UI and add seek to word onclick  feature

--- src/containers/Home/components/AudioControlBar.js
+++ src/containers/Home/components/AudioControlBar.js
@@ -1,19 +1,24 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, makeStyles } from "@material-ui/core";
 import recordingFile from "../../../assets/audios/recording.wav";
 import AudioPlayer from "../../../components/AudioPlayer";
 
+const useStyles = makeStyles((theme) => ({
+  headerWrapper: {
+    background: "#EFF3F6",
+    boxShadow: "none",
+  },
+}));
+
 const AudioControlBar = (props) => {
+  const classes = useStyles();
   return (
-    <AppBar>
+    <AppBar className={classes.headerWrapper}>
       <Toolbar>
-        <AudioPlayer
-          recordingFile={recordingFile}
-          {...props}
-        />
+        <AudioPlayer recordingFile={recordingFile} {...props} />
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default AudioControlBar;
--- src/containers/Home/components/PersonDialog.js
+++ src/containers/Home/components/PersonDialog.js
@@ -5,40 +5,131 @@ import { dialogSpeakers } from "./TranscriptContainer";
 const useStyles = makeStyles((theme) => ({
   transcriptBlockPersonOne: {
     display: "flex",
-    padding: "30px 30px 0px 30px",
+    padding: "30px 0px 0px",
+    alignItems: "center",
   },
+
   transcriptBlockPersonTwo: {
     display: "flex",
     padding: "30px 30px 0px 30px",
     marginLeft: "60px",
+    alignItems: "center",
   },
+
   transcriptSentenceContainer: {
     padding: "10px 10px 0",
   },
+
   transcriptSentence: {
     display: "inline",
+    color: "#556C86",
   },
+
   startTimeContainer: {
     padding: "10px 10px 0",
+    color: "#1A99F6",
+  },
+
+  startTimeContainerPersonOne: {
+    color: "#1A99F6",
+    padding: "0 10px 0 ",
+    fontWeight: "600",
+    fontFamily: "Proxima Nova Rg",
+  },
+
+  startTimeContainerPersonTwo: {
+    color: "#8868E9",
+    padding: "0 10px 0 ",
+    fontWeight: "600",
+    fontFamily: "Proxima Nova Rg",
+  },
+
+  transcriptSentenceContainerPersonOne: {
+    borderLeft: "2px rgba(13,119,198,.25) solid",
+    padding: "0 10px",
+    fontFamily: "Proxima Nova Rg",
+    fontWeight: "normal",
+  },
+
+  transcriptSentenceContainerPersonTwo: {
+    borderLeft: "2px rgba(136,104,233,.25) solid",
+    padding: "0 10px",
+    fontFamily: "Proxima Nova Rg",
+    fontWeight: "normal",
+  },
+
+  sentenceWord: {
+    "&:hover": {
+      borderRadius: "2px",
+      background: "rgba(26, 153, 246, 0.25)",
+    },
+  },
+
+  hightlightField: {
+    background: "rgba(26, 153, 246, 0.25)",
+    borderRadius: "2px",
+    "&:hover": {
+      background: "rgba(26, 153, 246, 0.25)",
+    },
   },
 }));
 
-const PersonDialog = ({ sentenceWords, dialogSpeaker }) => {
+const PersonDialog = ({
+  sentenceWords,
+  dialogSpeaker,
+  currentTime,
+  setSeekTime,
+}) => {
   const classes = useStyles();
-  const transcriptBlock =
-    dialogSpeaker === dialogSpeakers.PERSON_ONE
-      ? classes.transcriptBlockPersonOne
-      : classes.transcriptBlockPersonTwo;
+  let transcriptBlock = classes.transcriptBlockPersonOne;
+  let startTimeContainer = classes.startTimeContainerPersonOne;
+  let transcriptSentenceContainer =
+    classes.transcriptSentenceContainerPersonOne;
+
+  if (dialogSpeaker === dialogSpeakers.PERSON_TWO) {
+    transcriptBlock = classes.transcriptBlockPersonTwo;
+    startTimeContainer = classes.startTimeContainerPersonTwo;
+    transcriptSentenceContainer = classes.transcriptSentenceContainerPersonTwo;
+  }
+
+  const onWordClicked = (wordStartTime) => {
+    console.log("Hello World");
+    setSeekTime(wordStartTime);
+  };
 
   return (
     <div className={transcriptBlock}>
-      <div className={classes.startTimeContainer}>
-        {sentenceWords[0].startTime}
-      </div>
-      <div className={classes.transcriptSentenceContainer}>
+      <div className={startTimeContainer}>{sentenceWords[0].startTime}</div>
+      <div className={transcriptSentenceContainer}>
         <p className={classes.transcriptSentence}>
-          {sentenceWords.map((wordDetails) => {
-            return <span>{wordDetails.word} </span>;
+          {sentenceWords.map((wordDetails, index) => {
+            const wordStartTime = parseFloat(wordDetails.startTime);
+            let word = (
+              <React.Fragment key={index}>
+                <span
+                  className={classes.sentenceWord}
+                  onClick={() => onWordClicked(wordStartTime)}
+                >
+                  {wordDetails.word}
+                </span>{" "}
+              </React.Fragment>
+            );
+            if (
+              currentTime >= parseFloat(wordDetails.startTime) &&
+              currentTime <= parseFloat(wordDetails.endTime)
+            ) {
+              word = (
+                <React.Fragment key={index}>
+                  <span
+                    className={classes.hightlightField}
+                    onClick={() => onWordClicked(wordStartTime)}
+                  >
+                    {wordDetails.word}
+                  </span>{" "}
+                </React.Fragment>
+              );
+            }
+            return word;
           })}
         </p>
       </div>
