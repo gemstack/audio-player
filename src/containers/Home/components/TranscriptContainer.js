d4e3ae209f767913b4df10bfe10bc1b9de274c65
Sumit <sr682085@gmail.com>
2019-08-13 18:34:49 +0530

    Add share.png and break transcript component into - PersonDialog - SearchBar

--- src/containers/Home/components/TranscriptContainer.js
+++ src/containers/Home/components/TranscriptContainer.js
@@ -1,119 +1,37 @@
-import React from 'react';
-import SearchIcon from '@material-ui/icons/Search';
-import { fade, InputBase, makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+import PersonDialog from "./PersonDialog";
+import SearchBar from "./SearchBar";
 
 const useStyles = makeStyles((theme) => ({
   transcript: {
-    marginTop: '100px',
-  },
-  parentTranscriptBlock: {
-    display: 'flex',
-  },
-  childTranscriptBlock: {
-    display: 'flex',
-    padding: '20px 10px 0 0',
-  },
-  transcriptSentence: {
-    display: 'inline',
-  },
-  search: {
-    position: 'relative',
-    border: '1px solid black',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
+    marginTop: "100px",
   },
 }));
 
+export const dialogSpeakers = {
+  PERSON_ONE: 0,
+  PERSON_TWO: 1,
+};
+
 const TranscriptContainer = ({ transcript }) => {
   const classes = useStyles();
-  const personOneTranscripts = transcript.word_timings.filter(
-    (value, index) => index % 2 === 0
+  const transcriptSection = transcript.word_timings.map(
+    (sentenceWords, index) => (
+      <PersonDialog
+        sentenceWords={sentenceWords}
+        dialogSpeaker={
+          index % 2 === 0
+            ? dialogSpeakers.PERSON_ONE
+            : dialogSpeakers.PERSON_TWO
+        }
+      />
+    )
   );
-  const personTwoTranscripts = transcript.word_timings.filter(
-    (value, index) => index % 2 !== 0
-  );
-
-  let transcriptSection = personOneTranscripts.map((personOneSentenceWords,index) => {
-    const childSentenceWords = personTwoTranscripts.length > index ? personTwoTranscripts[index] : null;
-
-    return (
-      <div style={{ padding: '30px 30px 0px 30px' }}>
-        <div className={classes.parentTranscriptBlock}>
-          <div style={{ padding: '10px 10px 0' }}>
-            {personOneSentenceWords[0].startTime}
-          </div>
-          <div style={{ padding: '10px 10px 0' }}>
-            <p className={classes.transcriptSentence}>
-              {personOneSentenceWords.map((wordDetails) => {
-                return <span>{wordDetails.word} </span>;
-              })}
-            </p>
-            {childSentenceWords && (
-              <div className={classes.childTranscriptBlock}>
-                <div>{childSentenceWords[0].startTime}</div>
-                <div style={{ padding: '0px 10px 10px 20px' }}>
-                  <p className={classes.transcriptSentence}>
-                    {childSentenceWords.map((wordDetails) => {
-                      return <span>{wordDetails.word} </span>;
-                    })}
-                  </p>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  });
 
   return (
     <div className={classes.transcript}>
-      <div className={classes.search}>
-        <div className={classes.searchIcon}>
-          <SearchIcon />
-        </div>
-        <InputBase
-          placeholder='Search call transcript'
-          classes={{
-            root: classes.inputRoot,
-            input: classes.inputInput,
-          }}
-          inputProps={{ 'aria-label': 'search' }}
-        />
-      </div>
+      <SearchBar />
       <div>{transcriptSection}</div>
     </div>
   );
