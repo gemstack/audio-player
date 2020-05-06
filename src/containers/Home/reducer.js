69074cfef82d045c2031df90ef52d6c27c6736c3
Sumit <sr682085@gmail.com>
2020-05-06 20:55:11 +0530

    WIP

--- src/containers/Home/reducer.js
+++ src/containers/Home/reducer.js
@@ -2,13 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const configSlice = createSlice({
   name: "home",
-  initialState: {},
-  reducers: {},
+  initialState: {
+    audioControlBar: {
+      playing: true,
+    },
+  },
+  reducers: {
+    togglePlayPause(state, action) {
+      console.log("Hello Wrold");
+      state.audioControlBar.playing = !state.audioControlBar.playing;
+    },
+  },
 });
 
 // Extract the action creators object and the reducer
-const { actions, reducer: homeReducer } = configSlice;
+const { actions, reducer } = configSlice;
 // Extract and export each action creator by name
-export const {} = actions;
+export const { togglePlayPause } = actions;
 // Export the reducer, either as a default or named export
-export default homeReducer;
+export default reducer;
