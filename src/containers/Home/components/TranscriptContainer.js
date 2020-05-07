1df60dce625b0cddcfb9a75ed8d0af6fa821fed0
gemstack18 <gemstack18@gmail.com>
2020-05-07 10:36:23 +0530

    Lint fixes

--- src/containers/Home/components/TranscriptContainer.js
+++ src/containers/Home/components/TranscriptContainer.js
@@ -1,58 +1,57 @@
-import React from "react";
-import { connect } from "react-redux";
-import SearchIcon from "@material-ui/icons/Search";
-import { fade, InputBase, makeStyles } from "@material-ui/core";
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade, InputBase, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   transcript: {
-    marginTop: "100px",
+    marginTop: '100px',
   },
   parentTranscriptBlock: {
-    display: "flex",
+    display: 'flex',
   },
   childTranscriptBlock: {
-    display: "flex",
-    padding: "20px 10px 0 0",
+    display: 'flex',
+    padding: '20px 10px 0 0',
   },
   transcriptSentence: {
-    display: "inline",
+    display: 'inline',
   },
   search: {
-    position: "relative",
-    border: "1px solid black",
+    position: 'relative',
+    border: '1px solid black',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
+    '&:hover': {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
-    [theme.breakpoints.up("sm")]: {
+    [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(1),
-      width: "auto",
+      width: 'auto',
     },
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   inputRoot: {
-    color: "inherit",
+    color: 'inherit',
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
       },
     },
   },
@@ -71,12 +70,12 @@ const TranscriptContainer = ({ transcript }) => {
     const childSentenceWords = personTwoTranscripts.length > index ? personTwoTranscripts[index] : null;
 
     return (
-      <div style={{ padding: "30px 30px 0px 30px" }}>
+      <div style={{ padding: '30px 30px 0px 30px' }}>
         <div className={classes.parentTranscriptBlock}>
-          <div style={{ padding: "10px 10px 0" }}>
+          <div style={{ padding: '10px 10px 0' }}>
             {personOneSentenceWords[0].startTime}
           </div>
-          <div style={{ padding: "10px 10px 0" }}>
+          <div style={{ padding: '10px 10px 0' }}>
             <p className={classes.transcriptSentence}>
               {personOneSentenceWords.map((wordDetails) => {
                 return <span>{wordDetails.word} </span>;
@@ -85,7 +84,7 @@ const TranscriptContainer = ({ transcript }) => {
             {childSentenceWords && (
               <div className={classes.childTranscriptBlock}>
                 <div>{childSentenceWords[0].startTime}</div>
-                <div style={{ padding: "0px 10px 10px 20px" }}>
+                <div style={{ padding: '0px 10px 10px 20px' }}>
                   <p className={classes.transcriptSentence}>
                     {childSentenceWords.map((wordDetails) => {
                       return <span>{wordDetails.word} </span>;
@@ -107,12 +106,12 @@ const TranscriptContainer = ({ transcript }) => {
           <SearchIcon />
         </div>
         <InputBase
-          placeholder="Search call transcript"
+          placeholder='Search call transcript'
           classes={{
             root: classes.inputRoot,
             input: classes.inputInput,
           }}
-          inputProps={{ "aria-label": "search" }}
+          inputProps={{ 'aria-label': 'search' }}
         />
       </div>
       <div>{transcriptSection}</div>
@@ -120,13 +119,4 @@ const TranscriptContainer = ({ transcript }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  transcript: state.home.transcript,
-});
-
-const mapDispatchToProps = {};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TranscriptContainer);
+export default TranscriptContainer;
